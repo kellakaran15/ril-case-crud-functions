f0523e83696c199cd3ff630ee43e67f6f0cb2b5a
kellakaran15 <101532160+kellakaran15@users.noreply.github.com>
2022-04-06 13:22:37 +1000

    commit

---
+++ deleteCase/index.js
@@ -0,0 +1,17 @@
+const MongoClient = require('../api/dbConnect');
+const { config } = require('../config');
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const client = await MongoClient;
+
+    const db = await client.db(config.MONGO_DB);
+    const caseHeader = await db.collection("caseHeader").deleteOne(req.body);
+    const responseMessage = caseHeader;
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
